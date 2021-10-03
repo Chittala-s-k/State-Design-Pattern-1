9c0757f5e22efbab7cad9fd5afb5f7af2a32ebbe
Chittala-s-k <90112675+Chittala-s-k@users.noreply.github.com>
2021-10-03 08:19:15 +0530

    Add files via upload

---
+++ CloseState.java
@@ -0,0 +1,41 @@
+package state;
+
+
+public class CloseState implements State {
+	Door door;
+	public CloseState( Door operator ) {
+		this.door = operator;
+	}
+	public void doorOpen() {
+		
+		if(door.checkCondition()) {
+			System.out.println("Door is open");
+			door.setState(door.getOpenState());
+		}
+		else {
+			System.out.println("Repair Door as its broken");
+			door.setState(door.getOpenState());
+		}
+	}
+
+	public void doorClose() {
+		System.out.println("Door is closed");
+//		operator.setState(operator.getCloseState());
+	}
+
+	public void doorLock() {
+		System.out.println("Door is locked, Kindly Unlock");
+
+	}
+
+	public void doorUnlock() {
+		System.out.println("Door is Unlocked, Kindly Lock it");
+	}
+
+
+	public String toString() {
+		return " Closed";
+	}
+
+
+}
